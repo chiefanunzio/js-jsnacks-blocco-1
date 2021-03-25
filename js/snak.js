511ed09044a2bfe870cf5844e870aea36cb84c60
chiefanunzio <chiefa.nunzio@gmail.com>
2021-03-25 18:13:41 +0100

    cc

--- js/snak.js
+++ js/snak.js
@@ -2,6 +2,13 @@
 // zucchina, indicando per ognuna varietà, peso e lunghezza.
 // Calcola quanto pesano tutte le zucchine.
 
+// Crea 10 oggetti che rappresentano una zucchina.
+// Dividi in due array separati le zucchine che misurano
+// meno o più di 15 cm.
+// Infine stampa separatamente quanto pesano i due gruppi
+// di zucchine
+
+
 function lunghezza() {
     var zucchine = [
 
@@ -60,21 +67,39 @@ function peso(zucchina) {
 }
 
 
-lunghezza();
-peso();
 
-// Crea 10 oggetti che rappresentano una zucchina.
-// Dividi in due array separati le zucchine che misurano
-// meno o più di 15 cm.
-// Infine stampa separatamente quanto pesano i due gruppi
-// di zucchine
 
-// Scrivi una funzione che accetti una stringa come
-// argomento e la ritorni girata(es.Ciao - > oaiC)
 
 // Scrivi una funzione che fonda due array(aventi lo stesso numero di elementi) prendendo alternativamente gli
 // elementi da uno e dall 'altro
 // es. [a, b, c], [1, 2, 3]→[a, 1, b, 2, c, 3].
+function arr() {
+    
+    var lettere = ['a', 'b', 'c'];
+    var numeri = [1, 2, 3, ];
+    var all = [];
+    for (i = 0; i < lettere.length; i++) {
+        
+        var lett = lettere[i];
+        var num = numeri[i];
+        
+        
+        all.push(lett);
+        all.push(num);
+        
+        console.log(somma);
+    }
+    console.log(all);
+    
+}
+
+//  arr();
+// lunghezza();
+// peso();
+
+
+
+
 
 // Scrivi una funzione che accetti tre argomenti:
 //    un array e due numeri("a"
@@ -83,3 +108,13 @@ peso();
 //       grande al massimo quanto il numero di elementi dell 'array).
 //       La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra "a"
 //       e "b"
+
+function splitter(min, max, array) {}
+
+function esArraySplitter() {
+   var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+   var indMin = 3;
+   var indMax = 7;
+   var splitted = splitter(indMin, indMax, arr);
+   // [ 4, 5, 6, 7, 8 ]
+}
